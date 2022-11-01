bde2f09e2dc3b158470c72234cbbb5f967cfbe65
Caitlin Kolb <caitlinkolb@Caitlins-MacBook-Pro.local>
2022-11-01 19:57:37 -0400

    fixed tests cases to be in correct json format. Before had seperate lists like in the routes file, but adjusted so they are all in one list except for the invalid values and adjusted some incorrect types

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -21,6 +21,42 @@ def test_predict_route():
     url = '/predict'
  
     response = client.get(url)
- 
+    correctJson = [
+    {"school": "GP", "traveltime": 1, "studytime": 2, "schoolsup": "no", "famsup": "no", "paid": "yes", 
+    "activities": "yes", "higher": "yes", "internet": "yes", "freetime": 3, "dalc": 3, "walc": 4, "absences": 12},
+    {"school": "MS", "traveltime": 3, "studytime": 1, "schoolsup": "yes", "famsup": "yes", "paid": "yes", 
+    "activities": "yes", "higher": "yes", "internet": "yes", "freetime": 3, "dalc": 3, "walc": 5, "absences": 93},
+    {"school": "MS", "traveltime": 3, "studytime": 4, "schoolsup": "no", "famsup": "no", "paid": "yes", 
+    "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0},
+    {"school": "MS", "traveltime": 3, "studytime": 0, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
+    "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}, 
+    {"school": "GP", "traveltime": 0, "studytime": 0, "schoolsup": "no", "famsup": "no", "paid": "yes", 
+    "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}, 
+    {"school": "GP", "traveltime": 0, "studytime": 4, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
+    "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}, 
+    {"school": "GP", "traveltime": 0, "studytime": 4, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
+    "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}, 
+    {"school": 12, "traveltime": 0, "studytime": 4, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
+    "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}, 
+    ]
+
+    # invalid studytime 
+    badStudy =[{"school": "MS", "traveltime": 3, "studytime": -8, "schoolsup": "yes", "famsup": "yes", "paid": "yes", 
+    "activities": "yes", "higher": "yes", "internet": "yes", "freetime": 3, "dalc": 3, "walc": 5, "absences": 93}]
+    #invalid internet 
+    badInternet = [{"school": "MS", "traveltime": 3, "studytime": 8, "schoolsup": "yes", "famsup": "yes", "paid": "yes", 
+    "activities": "yes", "higher": "yes", "internet": "YES", "freetime": 3, "dalc": 3, "walc": 5, "absences": 93}]
+    #invalid schoolsup
+    badSchoolsUp = [{"school": "MS", "traveltime": 3, "studytime": 8, "schoolsup": "pl", "famsup": "yes", "paid": "yes", 
+    "activities": "yes", "higher": "yes", "internet": "yes", "freetime": 3, "dalc": 3, "walc": 5, "absences": 93}]
+    #invalid walc
+    badWalc = [{"school": 12, "traveltime": 0, "studytime": 4, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
+    "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": "yo", "absences": 0}
+   
+    
     assert response.status_code == 200
-    assert response.get_data()
+    assert response.get_data(currentJson) = {"prediction": [12, 9, 5, 18, 19, 3, 5, 7]}
+    assert response.get_data(badStudy) = {"prediction": [0]}
+    assert response.get_data(badInternet) = {"prediction": [0]}
+    assert response.get_data(badSchoolsUp) = {"prediction": [0]}
+    assert response.get_data(badWalc) = {"prediction": [0]}
