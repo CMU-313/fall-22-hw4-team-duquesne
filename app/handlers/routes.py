f746110affbf27ae26fcd262c68b34ef401e71b5
LunaFang1016 <Luna_Fang_nfls@163.com>
2022-11-07 22:27:39 -0500

    Built ML model from only numerical values

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -16,7 +16,7 @@ def configure_routes(app):
         return "try the predict route it is great!"
 
 
-    @app.route('/predict', methods = ['POST', 'GET'])
+    @app.route('/predict', methods = ['GET'])
     def predict():
         #use entries from the query string here but could also use json
         #useful data: school, traveltime, studytime, schoolsup, famsup, paid, 
@@ -24,106 +24,129 @@ def configure_routes(app):
         #             absences
         # age = request.args.get('age')
         # health = request.args.get('health')
-        school = request.args.get('school')
+        absences = request.args.get('absences')
         traveltime = request.args.get('travaltime')
         studytime = request.args.get('studytime')
-        schoolsup = request.args.get('schoolsup')
-        famsup = request.args.get('famsup')
-        paid = request.args.get('paid')
-        activities = request.args.get('activities')
-        higher = request.args.get('higher')
-        internet = request.args.get('internet')
-        freetime = request.args.get('freetime')
         dalc = request.args.get('Dalc')
         walc = request.args.get('Walc')
-        absences = request.args.get('absences')
-        schoolsup_no, schoolsup_yes, schoolsup_nan = 0,0
-        if schoolsup == 'yes':
-            schoolsup_yes = 1
-        elif schoolsup == 'no':
-            schoolsup_no = 1
-        else:
-            schoolsup_nan = 1
-        
-        activities_no, activities_yes, actitvities_nan = 0,0,0
-
-        if activities == 'yes':
-            activities_yes = 1
-        elif activities == 'no':
-            activities_no = 1
-        else:
-            activities_nan = 1
-
-        famsup_no, famsup_yes, famsup_nan = 0,0,0
-        if famsup == 'yes':
-            famsup_yes = 1
-        elif famsup == 'no':
-            famsup_no = 1
-        else:
-            famsup_nan = 1
-        
-
-        higher_no, higher_yes, famsup_nan = 0,0,0
-        if higher == 'yes':
-            higher_yes = 1
-        elif higher == 'no':
-            higher_no = 1
-        else:
-            higher_nan = 1
-
-        internet_no, internet_yes, internet_nan = 0,0,0
-        if internet == 'yes':
-            internet_yes = 1
-        elif internet == 'no':
-            internet_no = 1
-        else:
-            internet_nan = 1
-        
-        paid_no, paid_yes, paid_nan = 0,0,0
-        if paid == 'yes':
-            paid_yes = 1
-        elif paid == 'no':
-            paid_no = 1
-        else:
-            paid_nan = 1
-        
-        school_GP, school_MS, school_nan = 0,0,0
-        if school == 'GP':
-            school_GP = 1
-        elif school == 'MS':
-            school_MS = 1
-        else:
-            school_nan = 1
-
-        data = [[dalc], [walc], [absences], [freetime], [studytime], 
-                [traveltime], [activities_no], [activities_yes], 
-                [famsup_no], [famsup_yes], [higher_no], [higher_yes],
-                [internet_no], [internet_yes], [paid_no], [paid_yes],
-                [school_GP], [school_MS], [schoolsup_no], [schoolsup_yes]]
-        query_df = pd.DataFrame({'Dalc': pd.Series(dalc), 'Walc': pd.Series(walc), 'absences': pd.Series(absences), 'freetime': pd.Series(freetime),
-                          'studytime': pd.Series(studytime), 'traveltime': pd.Series(traveltime),
-                          'activities_no': pd.Series(activities_no), 'activities_yes': pd.Series(activities_yes),
-                          'famsup_no': pd.Series(famsup_no), 'famsup_yes': pd.Series(famsup_yes), 
-                          'higher_no': pd.Series(higher_no), 'higher_yes': pd.Series(higher_yes),
-                          'internet_no': pd.Series(internet_no), 'internet_yes': pd.Series(internet_yes),
-                          'paid_no': pd.Series(paid_no), 'paid_yes': pd.Series(paid_yes),
-                          'school_GP' : pd.Series(school_GP), 'school_MS': pd.Series(school_MS),
-                          'schoolsup_no': pd.Series(schoolsup_no), 'schoolsup_yes': pd.Series(schoolsup_yes)} )
-        # query_df = pd.DataFrame({
-        #     'school': pd.Series(school),
-        #     'traveltime': pd.Series(traveltime),
-        #     'studytime': pd.Series(studytime),
-        #     'schoolsup': pd.Series(famsup),
-        #     'paid': pd.Series(paid),
-        #     'activities': pd.Series(activities),
-        #     'higher': pd.Series(higher),
-        #     'internet': pd.Series(internet),
-        #     'freetime': pd.Series(freetime),
-        #     'Dalc': pd.Series(dalc),
-        #     'Walc': pd.Series(walc),
-        #     'absences': pd.Series(absences)
-        # })
+        freetime = request.args.get('freetime')
+        print(absences)
+        data = [[traveltime], [studytime],[freetime],[dalc],[walc], [absences]]
+        query_df = pd.DataFrame(data = {
+            'traveltime': pd.Series(traveltime),
+            'studytime': pd.Series(studytime),
+            'freetime': pd.Series(freetime),
+            'Dalc': pd.Series(dalc),
+            'Walc': pd.Series(walc),
+            'absences': pd.Series(absences)
+            },
+            index = ['traveltime', 'studytime', 'freetime', 'Dalc', 'Walc', 'absences'])
         query = pd.get_dummies(query_df)
+        print(query_df)
         print(query)
         prediction = clf.predict(query)
         return jsonify(np.asscalar(prediction))
+        # school = request.args.get('school')
+        # traveltime = request.args.get('travaltime')
+        # studytime = request.args.get('studytime')
+        # schoolsup = request.args.get('schoolsup')
+        # famsup = request.args.get('famsup')
+        # paid = request.args.get('paid')
+        # activities = request.args.get('activities')
+        # higher = request.args.get('higher')
+        # internet = request.args.get('internet')
+        # freetime = request.args.get('freetime')
+        # dalc = request.args.get('Dalc')
+        # walc = request.args.get('Walc')
+        
+        # schoolsup_no, schoolsup_yes, schoolsup_nan = 0,0
+        # if schoolsup == 'yes':
+        #     schoolsup_yes = 1
+        # elif schoolsup == 'no':
+        #     schoolsup_no = 1
+        # else:
+        #     schoolsup_nan = 1
+        
+        # activities_no, activities_yes, actitvities_nan = 0,0,0
+
+        # if activities == 'yes':
+        #     activities_yes = 1
+        # elif activities == 'no':
+        #     activities_no = 1
+        # else:
+        #     activities_nan = 1
+
+        # famsup_no, famsup_yes, famsup_nan = 0,0,0
+        # if famsup == 'yes':
+        #     famsup_yes = 1
+        # elif famsup == 'no':
+        #     famsup_no = 1
+        # else:
+        #     famsup_nan = 1
+        
+
+        # higher_no, higher_yes, famsup_nan = 0,0,0
+        # if higher == 'yes':
+        #     higher_yes = 1
+        # elif higher == 'no':
+        #     higher_no = 1
+        # else:
+        #     higher_nan = 1
+
+        # internet_no, internet_yes, internet_nan = 0,0,0
+        # if internet == 'yes':
+        #     internet_yes = 1
+        # elif internet == 'no':
+        #     internet_no = 1
+        # else:
+        #     internet_nan = 1
+        
+        # paid_no, paid_yes, paid_nan = 0,0,0
+        # if paid == 'yes':
+        #     paid_yes = 1
+        # elif paid == 'no':
+        #     paid_no = 1
+        # else:
+        #     paid_nan = 1
+        
+        # school_GP, school_MS, school_nan = 0,0,0
+        # if school == 'GP':
+        #     school_GP = 1
+        # elif school == 'MS':
+        #     school_MS = 1
+        # else:
+        #     school_nan = 1
+
+        # data = [[dalc], [walc], [absences], [freetime], [studytime], 
+        #         [traveltime], [activities_no], [activities_yes], 
+        #         [famsup_no], [famsup_yes], [higher_no], [higher_yes],
+        #         [internet_no], [internet_yes], [paid_no], [paid_yes],
+        #         [school_GP], [school_MS], [schoolsup_no], [schoolsup_yes]]
+        # query_df = pd.DataFrame({'Dalc': pd.Series(dalc), 'Walc': pd.Series(walc), 'absences': pd.Series(absences), 'freetime': pd.Series(freetime),
+        #                   'studytime': pd.Series(studytime), 'traveltime': pd.Series(traveltime),
+        #                   'activities_no': pd.Series(activities_no), 'activities_yes': pd.Series(activities_yes),
+        #                   'famsup_no': pd.Series(famsup_no), 'famsup_yes': pd.Series(famsup_yes), 
+        #                   'higher_no': pd.Series(higher_no), 'higher_yes': pd.Series(higher_yes),
+        #                   'internet_no': pd.Series(internet_no), 'internet_yes': pd.Series(internet_yes),
+        #                   'paid_no': pd.Series(paid_no), 'paid_yes': pd.Series(paid_yes),
+        #                   'school_GP' : pd.Series(school_GP), 'school_MS': pd.Series(school_MS),
+        #                   'schoolsup_no': pd.Series(schoolsup_no), 'schoolsup_yes': pd.Series(schoolsup_yes)} )
+        # # query_df = pd.DataFrame({
+        # #     'school': pd.Series(school),
+        # #     'traveltime': pd.Series(traveltime),
+        # #     'studytime': pd.Series(studytime),
+        # #     'schoolsup': pd.Series(famsup),
+        # #     'paid': pd.Series(paid),
+        # #     'activities': pd.Series(activities),
+        # #     'higher': pd.Series(higher),
+        # #     'internet': pd.Series(internet),
+        # #     'freetime': pd.Series(freetime),
+        # #     'Dalc': pd.Series(dalc),
+        # #     'Walc': pd.Series(walc),
+        # #     'absences': pd.Series(absences)
+        # # })
+        # query = pd.get_dummies(query_df)
+        # print(query)
+        # prediction = clf.predict(query)
+        # return jsonify(np.asscalar(prediction))
+        
--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -10,7 +10,7 @@ def test_base_route():
     url = '/'
 
     response = client.get(url)
-
+    
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
 
@@ -21,41 +21,53 @@ def test_predict_route():
     url = '/predict'
  
     response = client.get(url)
-    correctJson = [
-    {"school": "GP", "traveltime": 1, "studytime": 2, "schoolsup": "no", "famsup": "no", "paid": "yes", 
-    "activities": "yes", "higher": "yes", "internet": "yes", "freetime": 3, "dalc": 3, "walc": 4, "absences": 12},
-    {"school": "MS", "traveltime": 3, "studytime": 1, "schoolsup": "yes", "famsup": "yes", "paid": "yes", 
-    "activities": "yes", "higher": "yes", "internet": "yes", "freetime": 3, "dalc": 3, "walc": 5, "absences": 93},
-    {"school": "MS", "traveltime": 3, "studytime": 4, "schoolsup": "no", "famsup": "no", "paid": "yes", 
-    "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0},
-    {"school": "MS", "traveltime": 3, "studytime": 0, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
-    "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}, 
-    {"school": "GP", "traveltime": 1, "studytime": 0, "schoolsup": "no", "famsup": "no", "paid": "yes", 
-    "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}, 
-    {"school": "GP", "traveltime": 2, "studytime": 4, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
-    "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}, 
-    {"school": "GP", "traveltime": 1, "studytime": 4, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
-    "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}, 
-    {"school": "MS", "traveltime": 3, "studytime": 4, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
-    "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}
-    ]
+    correctJson = [{"traveltime": 2, "studytime": 2, "freetime": 3, 
+                    "Dalc": 1, "Walc": 1, "absences": 6}, # line 1, G3 = 6
+                   {"traveltime": 1, "studytime": 3, "freetime": 2, 
+                    "Dalc": 1, "Walc": 1, "absences": 5}, # line 5, G3 = 15
+                   {"traveltime": 1, "studytime": 2, "freetime": 3, 
+                    "Dalc": 1, "Walc": 2, "absences": 0}, # line 12, G3 = 9
+                  ]
 
-    # invalid studytime 
-    badStudy = [{"school": "MS", "traveltime": 3, "studytime": -8, "schoolsup": "yes", "famsup": "yes", "paid": "yes", 
-    "activities": "yes", "higher": "yes", "internet": "yes", "freetime": 3, "dalc": 3, "walc": 5, "absences": 93}]
-    #invalid internet 
-    badInternet = [{"school": "MS", "traveltime": 3, "studytime": 8, "schoolsup": "yes", "famsup": "yes", "paid": "yes", 
-    "activities": "yes", "higher": "yes", "internet": "YES", "freetime": 3, "dalc": 3, "walc": 5, "absences": 93}]
-    #invalid schoolsup
-    badSchoolsUp = [{"school": "MS", "traveltime": 3, "studytime": 8, "schoolsup": "pl", "famsup": "yes", "paid": "yes", 
-    "activities": "yes", "higher": "yes", "internet": "yes", "freetime": 3, "dalc": 3, "walc": 5, "absences": 93}]
-    #invalid walc
-    badWalc = [{"school": 12, "traveltime": 0, "studytime": 4, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
-    "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": "yo", "absences": 0}]
-   
     assert response.status_code == 200
-    assert response.post(url, data=correctJson) == {"prediction": [12, 9, 5, 18, 19, 3, 5, 7]}
-    assert response.post(url, data=badStudy) == {"prediction": [0]}
-    assert response.post(url, data=badInternet) == {"prediction": [0]}
-    assert response.post(url, data=badSchoolsUp) == {"prediction": [0]}
-    assert response.post(url, data=badWalc) == {"prediction": [0]}
+    assert response.post(url, data=correctJson) == {"prediction": [{6}, {15}, {9}]}
+
+    # correctJson = [
+    # {"school": "GP", "traveltime": 1, "studytime": 2, "schoolsup": "no", "famsup": "no", "paid": "yes", 
+    # "activities": "yes", "higher": "yes", "internet": "yes", "freetime": 3, "dalc": 3, "walc": 4, "absences": 12},
+    # {"school": "MS", "traveltime": 3, "studytime": 1, "schoolsup": "yes", "famsup": "yes", "paid": "yes", 
+    # "activities": "yes", "higher": "yes", "internet": "yes", "freetime": 3, "dalc": 3, "walc": 5, "absences": 93},
+    # {"school": "MS", "traveltime": 3, "studytime": 4, "schoolsup": "no", "famsup": "no", "paid": "yes", 
+    # "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0},
+    # {"school": "MS", "traveltime": 3, "studytime": 0, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
+    # "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}, 
+    # {"school": "GP", "traveltime": 1, "studytime": 0, "schoolsup": "no", "famsup": "no", "paid": "yes", 
+    # "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}, 
+    # {"school": "GP", "traveltime": 2, "studytime": 4, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
+    # "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}, 
+    # {"school": "GP", "traveltime": 1, "studytime": 4, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
+    # "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}, 
+    # {"school": "MS", "traveltime": 3, "studytime": 4, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
+    # "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": 4, "absences": 0}
+    # ]
+
+    # # invalid studytime 
+    # badStudy = [{"school": "MS", "traveltime": 3, "studytime": -8, "schoolsup": "yes", "famsup": "yes", "paid": "yes", 
+    # "activities": "yes", "higher": "yes", "internet": "yes", "freetime": 3, "dalc": 3, "walc": 5, "absences": 93}]
+    # #invalid internet 
+    # badInternet = [{"school": "MS", "traveltime": 3, "studytime": 8, "schoolsup": "yes", "famsup": "yes", "paid": "yes", 
+    # "activities": "yes", "higher": "yes", "internet": "YES", "freetime": 3, "dalc": 3, "walc": 5, "absences": 93}]
+    # #invalid schoolsup
+    # badSchoolsUp = [{"school": "MS", "traveltime": 3, "studytime": 8, "schoolsup": "pl", "famsup": "yes", "paid": "yes", 
+    # "activities": "yes", "higher": "yes", "internet": "yes", "freetime": 3, "dalc": 3, "walc": 5, "absences": 93}]
+    # #invalid walc
+    # badWalc = [{"school": 12, "traveltime": 0, "studytime": 4, "schoolsup": "yes", "famsup": "no", "paid": "yes", 
+    # "activities": "no", "higher": "no", "internet": "no", "freetime": 3, "dalc": 3, "walc": "yo", "absences": 0}]
+   
+    # assert response.status_code == 200
+    
+    # assert response.post(url, data=correctJson) == {"prediction": [12, 9, 5, 18, 19, 3, 5, 7]}
+    # assert response.post(url, data=badStudy) == {"prediction": [0]}
+    # assert response.post(url, data=badInternet) == {"prediction": [0]}
+    # assert response.post(url, data=badSchoolsUp) == {"prediction": [0]}
+    # assert response.post(url, data=badWalc) == {"prediction": [0]}
