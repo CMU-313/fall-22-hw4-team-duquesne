58ccadedf14e25bb7f0db3135a3384f961a408a3
LunaFang1016 <Luna_Fang_nfls@163.com>
2022-11-08 20:20:23 -0500

    Added test for valid requests and modified ML model (#5)
    * Added convertions to transform categorical data to numerical ones
    
    * Added convertions to transform categorical data to numerical ones
    
    * Added one successful url
    
    * Added one valid test
    
    * "Updated ML frame"
    
    * Added invalid case for test
    
    * Added tests for invalid values and updated relevant fields in ML model (#6)
    
    * Tested on missing parameters
    
    * Added test for request with incorrect form

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -18,135 +18,118 @@ def configure_routes(app):
 
     @app.route('/predict', methods = ['POST', 'GET'])
     def predict():
-        #use entries from the query string here but could also use json
         #useful data: school, traveltime, studytime, schoolsup, famsup, paid, 
         #             activities, higher, internet, freetime, Dalc, Walc,
         #             absences
-        # age = request.args.get('age')
-        # health = request.args.get('health')
+        # numerical values
         absences = request.args.get('absences')
-        traveltime = request.args.get('travaltime')
+        traveltime = request.args.get('traveltime')
         studytime = request.args.get('studytime')
         dalc = request.args.get('Dalc')
         walc = request.args.get('Walc')
         freetime = request.args.get('freetime')
-        print(absences)
-        data = [[traveltime], [studytime],[freetime],[dalc],[walc], [absences]]
+        # non-numerical values - categorical
+        school = request.args.get('school')
+        schoolsup = request.args.get('schoolsup')
+        famsup = request.args.get('famsup')
+        paid = request.args.get('paid')
+        activities = request.args.get('activities')
+        higher = request.args.get('higher')
+        internet = request.args.get('internet')
+        
+        schoolsup_no, schoolsup_yes, schoolsup_nan = 0,0,0
+        if schoolsup == 'yes':
+            schoolsup_yes = 1
+        elif schoolsup == 'no':
+            schoolsup_no = 1
+        else:
+            return 'Invalid school support status: expected yes or no'
+        
+        activities_no, activities_yes, actitvities_nan = 0,0,0
+
+        if activities == 'yes':
+            activities_yes = 1
+        elif activities == 'no':
+            activities_no = 1
+        else:
+            return 'Invalid activities status: expected yes or no'
+
+        famsup_no, famsup_yes, famsup_nan = 0,0,0
+        if famsup == 'yes':
+            famsup_yes = 1
+        elif famsup == 'no':
+            famsup_no = 1
+        else:
+            return 'Invalid family support status: expected yes or no'
+        
+        higher_no, higher_yes, famsup_nan = 0,0,0
+        if higher == 'yes':
+            higher_yes = 1
+        elif higher == 'no':
+            higher_no = 1
+        else:
+            return 'Invalid higher education status: expected yes or no'
+
+        internet_no, internet_yes, internet_nan = 0,0,0
+        if internet == 'yes':
+            internet_yes = 1
+        elif internet == 'no':
+            internet_no = 1
+        else:
+            return 'Invalid internet status: expected yes or no'
+        
+        paid_no, paid_yes, paid_nan = 0,0,0
+        if paid == 'yes':
+            paid_yes = 1
+        elif paid == 'no':
+            paid_no = 1
+        else:
+            return 'Invalid paid status: expected yes or no'
+        
+        school_GP, school_MS, school_nan = 0,0,0
+        if school == 'GP':
+            school_GP = 1
+        elif school == 'MS':
+            school_MS = 1
+        else:
+            return 'Invalid school: expected MS or GP'
+
+        data = [[dalc], [walc], [absences],
+                [freetime], [studytime], [traveltime], 
+                [activities_no], [activities_yes], 
+                [famsup_no], [famsup_yes], 
+                [higher_no], [higher_yes], 
+                [internet_no], [internet_yes], 
+                [paid_no], [paid_yes], 
+                [school_GP], [school_MS], 
+                [schoolsup_no], [schoolsup_yes]]
+
         query_df = pd.DataFrame(data = {
-            'traveltime': pd.Series(traveltime),
-            'studytime': pd.Series(studytime),
-            'freetime': pd.Series(freetime),
             'Dalc': pd.Series(dalc),
             'Walc': pd.Series(walc),
-            'absences': pd.Series(absences)
+            'absences': pd.Series(absences),
+            'freetime': pd.Series(freetime),
+            'studytime': pd.Series(studytime),
+            'traveltime': pd.Series(traveltime),
+            'activities_no': pd.Series(activities_no),
+            'activities_yes': pd.Series(activities_yes),
+            'famsup_no': pd.Series(famsup_no),
+            'famsup_yes': pd.Series(famsup_yes),
+            'higher_no': pd.Series(higher_no),
+            'higher_yes': pd.Series(higher_yes),
+            'internet_no': pd.Series(internet_no),
+            'internet_yes': pd.Series(internet_yes),
+            'paid_no': pd.Series(paid_no),
+            'paid_yes': pd.Series(paid_yes),
+            'school_GP': pd.Series(school_GP),
+            'school_MS': pd.Series(school_MS),
+            'schoolsup_no': pd.Series(schoolsup_no),
+            'schoolsup_yes': pd.Series(schoolsup_yes)
             },
-            index = ['traveltime', 'studytime', 'freetime', 'Dalc', 'Walc', 'absences'])
-        query = pd.get_dummies(query_df)
-        print(query_df)
-        print(query)
-        prediction = clf.predict(query)
-        return jsonify(np.asscalar(prediction))
-        # school = request.args.get('school')
-        # traveltime = request.args.get('travaltime')
-        # studytime = request.args.get('studytime')
-        # schoolsup = request.args.get('schoolsup')
-        # famsup = request.args.get('famsup')
-        # paid = request.args.get('paid')
-        # activities = request.args.get('activities')
-        # higher = request.args.get('higher')
-        # internet = request.args.get('internet')
-        # freetime = request.args.get('freetime')
-        # dalc = request.args.get('Dalc')
-        # walc = request.args.get('Walc')
-        
-        # schoolsup_no, schoolsup_yes, schoolsup_nan = 0,0
-        # if schoolsup == 'yes':
-        #     schoolsup_yes = 1
-        # elif schoolsup == 'no':
-        #     schoolsup_no = 1
-        # else:
-        #     schoolsup_nan = 1
-        
-        # activities_no, activities_yes, actitvities_nan = 0,0,0
-
-        # if activities == 'yes':
-        #     activities_yes = 1
-        # elif activities == 'no':
-        #     activities_no = 1
-        # else:
-        #     activities_nan = 1
-
-        # famsup_no, famsup_yes, famsup_nan = 0,0,0
-        # if famsup == 'yes':
-        #     famsup_yes = 1
-        # elif famsup == 'no':
-        #     famsup_no = 1
-        # else:
-        #     famsup_nan = 1
-        
-
-        # higher_no, higher_yes, famsup_nan = 0,0,0
-        # if higher == 'yes':
-        #     higher_yes = 1
-        # elif higher == 'no':
-        #     higher_no = 1
-        # else:
-        #     higher_nan = 1
-
-        # internet_no, internet_yes, internet_nan = 0,0,0
-        # if internet == 'yes':
-        #     internet_yes = 1
-        # elif internet == 'no':
-        #     internet_no = 1
-        # else:
-        #     internet_nan = 1
-        
-        # paid_no, paid_yes, paid_nan = 0,0,0
-        # if paid == 'yes':
-        #     paid_yes = 1
-        # elif paid == 'no':
-        #     paid_no = 1
-        # else:
-        #     paid_nan = 1
-        
-        # school_GP, school_MS, school_nan = 0,0,0
-        # if school == 'GP':
-        #     school_GP = 1
-        # elif school == 'MS':
-        #     school_MS = 1
-        # else:
-        #     school_nan = 1
-
-        # data = [[dalc], [walc], [absences], [freetime], [studytime], 
-        #         [traveltime], [activities_no], [activities_yes], 
-        #         [famsup_no], [famsup_yes], [higher_no], [higher_yes],
-        #         [internet_no], [internet_yes], [paid_no], [paid_yes],
-        #         [school_GP], [school_MS], [schoolsup_no], [schoolsup_yes]]
-        # query_df = pd.DataFrame({'Dalc': pd.Series(dalc), 'Walc': pd.Series(walc), 'absences': pd.Series(absences), 'freetime': pd.Series(freetime),
-        #                   'studytime': pd.Series(studytime), 'traveltime': pd.Series(traveltime),
-        #                   'activities_no': pd.Series(activities_no), 'activities_yes': pd.Series(activities_yes),
-        #                   'famsup_no': pd.Series(famsup_no), 'famsup_yes': pd.Series(famsup_yes), 
-        #                   'higher_no': pd.Series(higher_no), 'higher_yes': pd.Series(higher_yes),
-        #                   'internet_no': pd.Series(internet_no), 'internet_yes': pd.Series(internet_yes),
-        #                   'paid_no': pd.Series(paid_no), 'paid_yes': pd.Series(paid_yes),
-        #                   'school_GP' : pd.Series(school_GP), 'school_MS': pd.Series(school_MS),
-        #                   'schoolsup_no': pd.Series(schoolsup_no), 'schoolsup_yes': pd.Series(schoolsup_yes)} )
-        # # query_df = pd.DataFrame({
-        # #     'school': pd.Series(school),
-        # #     'traveltime': pd.Series(traveltime),
-        # #     'studytime': pd.Series(studytime),
-        # #     'schoolsup': pd.Series(famsup),
-        # #     'paid': pd.Series(paid),
-        # #     'activities': pd.Series(activities),
-        # #     'higher': pd.Series(higher),
-        # #     'internet': pd.Series(internet),
-        # #     'freetime': pd.Series(freetime),
-        # #     'Dalc': pd.Series(dalc),
-        # #     'Walc': pd.Series(walc),
-        # #     'absences': pd.Series(absences)
-        # # })
+            index = [0])
         # query = pd.get_dummies(query_df)
         # print(query)
-        # prediction = clf.predict(query)
-        # return jsonify(np.asscalar(prediction))
+        prediction = clf.predict(query_df)
+        return jsonify(np.ndarray.item(prediction))
+
         
